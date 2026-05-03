89df12e04f7061fb5b9adfcbb8656ae994f7d602
MdAbuSayed0188 <abu400061@gmail.com>
2026-05-03 23:17:53 +0600

    second commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const signinBtn = document.getElementById("signin-btn");
+console.log(signinBtn);
+signinBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const usernameValue = document.getElementById("username").value;
+    const passwordValue = document.getElementById("password").value;
+
+    if (!usernameValue || !passwordValue) {
+        alert("please provide username and password");
+    }
+    else if (usernameValue === "admin" && passwordValue === "admin123") {
+        
+        
+    }
+    else {
+        alert("Invalid username or password");
+    }
+});
